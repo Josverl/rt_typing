160f2887b954ce73be6b8219d676c408e0e1e4ef
Jos Verlinde <Jos.Verlinde@microsoft.com>
2024-08-30 21:55:14 +0200

    Quick Proto of minimal typing module
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

---
+++ mp_typing_min.py
@@ -0,0 +1,49 @@
+"""
+This module provides runtime support for type hints.	
+based on : https://github.com/micropython/micropython-lib/pull/584
+"""
+
+
+def cast(type, val):
+    return val
+
+
+def get_origin(type):
+    return None
+
+
+def get_args(type):
+    return ()
+
+def no_type_check(arg):
+    return arg
+
+
+
+def overload(func):
+    return None
+
+def NewType(name, type):
+    return type
+
+class _AnyCall:
+    """A class to ignore type hints in code."""
+
+    def __init__(*args, **kwargs):
+        pass
+
+    def __call__(*args, **kwargs):
+        pass
+
+    def __getitem__(self, arg):
+        return _any_call
+
+
+_any_call = _AnyCall()
+
+TYPE_CHECKING = False
+
+
+# ref: https://github.com/micropython/micropython-lib/pull/584#issuecomment-2317690854
+def __getattr__(attr):
+    return _any_call
